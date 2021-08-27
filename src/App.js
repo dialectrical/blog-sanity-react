cf3f2b1185ac8c1658bad420ba2b7779700427f0
dialectrical <>
2021-08-27 16:48:07 -0700

    switches to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import AllPosts from "./components/AllPosts.js";
 import OnePost from "./components/OnePost.js";
 import Archive from "./components/Archive.js";
@@ -9,13 +9,13 @@ function App() {
   return (
     <div>
       <NavBar />
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
           <Route component={AllPosts} path="/" exact />
-          <Route component={Archive} path="/archive" />
+          <Route component={Archive} path="/archive" exact />
           <Route component={OnePost} path="/:slug" />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
