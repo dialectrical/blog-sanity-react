f169aad750853ae63a9ffae7559779ce78d8c91d
dialectrical <>
2021-08-26 21:28:40 -0700

    adds category to OnePost.js

--- src/components/OnePost.js
+++ src/components/OnePost.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import sanityClient from "../client.js";
+import { Timestamp } from "./Timestamper.js";
+import Emojify from "./CategoryEmojifier.js";
 import BlockContent from "@sanity/block-content-to-react";
 import imageUrlBuilder from "@sanity/image-url";
 import { Container, Row, Col, Spinner } from "reactstrap";
@@ -26,7 +28,11 @@ export default function OnePost() {
               url
             }
           },
-        body
+          categories[0] -> {
+            title
+          },
+        body,
+        publishedAt
       }`,
         { slug }
       )
@@ -36,8 +42,8 @@ export default function OnePost() {
 
   if (!postData)
     return (
-      <Container>
-        <Spinner />
+      <Container style={{ padding: "10% 50% 50% 50%" }}>
+        <Spinner children="" />
       </Container>
     );
 
@@ -46,8 +52,17 @@ export default function OnePost() {
       <Row>
         <Row>
           <Col>
-            <h2>{postData.title}</h2>
+            <h2>
+              {Emojify(postData.categories)}
+              {postData.title}
+            </h2>
           </Col>
+        </Row>
+        <Row>
+          <p className="text-muted">
+            Posted at {Timestamp(postData.publishedAt)} to{" "}
+            {postData.categories.title}
+          </p>
         </Row>
         <Row>
           <Col>
